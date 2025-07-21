91848ecd786cbdaa199b605127a6a35f17ab9470
Bogdan <bogdanarizancu@gmail.com>
2025-07-22 00:17:34 +0200

    fix cors

--- api/views.js
+++ api/views.js
@@ -7,11 +7,13 @@ const notion = new Client({
 const DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
 export default async function handler(req, res) {
-    // Enable CORS
+    // Enable CORS for all origins
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Max-Age', '86400');
 
+    // Handle preflight request
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
